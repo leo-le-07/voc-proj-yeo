9cc157542cba480c784fea7e77b2a5eef011d409
Leo Le <coder.leo.le@gmail.com>
2016-05-18 23:12:52 +0700

    Initialize

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vocProjYeoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vocProjYeoApp
+ */
+angular.module('vocProjYeoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vocProjYeoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vocProjYeoApp
+ */
+angular.module('vocProjYeoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
